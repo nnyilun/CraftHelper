b19e6d483c19d069c2c3885ce9d87e99fac64a0c
JiangYilun <nnyilun@126.com>
2023-07-25 16:35:51 +0800

    7-25后端更新游戏职业数据，前端搜索页制作。

---
+++ Models/SearchModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CraftHelper.Models
+{
+    public class SearchModel : BaseModel
+    {
+        private SearchModel() { }
+
+        public static List<SearchInfo> SearchResult = new List<SearchInfo>() 
+        { 
+            new SearchInfo() { ItemName = "Unknown", ItemId = -1},
+            new SearchInfo() { ItemName = "Unknown", ItemId = -1},
+            new SearchInfo() { ItemName = "Unknown", ItemId = -1},
+            new SearchInfo() { ItemName = "Unknown", ItemId = -1},
+            new SearchInfo() { ItemName = "Unknown", ItemId = -1},
+            new SearchInfo() { ItemName = "Unknown", ItemId = -1},
+            new SearchInfo() { ItemName = "Unknown", ItemId = -1},
+            new SearchInfo() { ItemName = "Unknown", ItemId = -1},
+            new SearchInfo() { ItemName = "Unknown", ItemId = -1},
+            new SearchInfo() { ItemName = "Unknown", ItemId = -1},
+            new SearchInfo() { ItemName = "Unknown", ItemId = -1},
+            new SearchInfo() { ItemName = "Unknown", ItemId = -1},
+            new SearchInfo() { ItemName = "Unknown", ItemId = -1},
+            new SearchInfo() { ItemName = "Unknown", ItemId = -1},
+            new SearchInfo() { ItemName = "Unknown", ItemId = -1},
+            new SearchInfo() { ItemName = "Unknown", ItemId = -1},
+            new SearchInfo() { ItemName = "Unknown", ItemId = -1},
+            new SearchInfo() { ItemName = "Unknown", ItemId = -1},
+            new SearchInfo() { ItemName = "Unknown", ItemId = -1},
+            new SearchInfo() { ItemName = "Unknown", ItemId = -1},
+            new SearchInfo() { ItemName = "Unknown", ItemId = -1},
+            new SearchInfo() { ItemName = "Unknown", ItemId = -1},
+            new SearchInfo() { ItemName = "Unknown", ItemId = -1},
+            new SearchInfo() { ItemName = "Unknown", ItemId = -1},
+            new SearchInfo() { ItemName = "Unknown", ItemId = -1},
+            new SearchInfo() { ItemName = "Unknown", ItemId = -1},
+            new SearchInfo() { ItemName = "Unknown", ItemId = -1},
+            new SearchInfo() { ItemName = "Unknown", ItemId = -1},
+            new SearchInfo() { ItemName = "Unknown", ItemId = -1},
+            new SearchInfo() { ItemName = "Unknown", ItemId = -1},
+            new SearchInfo() { ItemName = "Unknown", ItemId = -1},
+            new SearchInfo() { ItemName = "Unknown", ItemId = -1},
+        };
+
+        public static List<SearchInfo> SearchAddition = new List<SearchInfo>();
+
+
+
+        public class SearchInfo
+        {
+            public static implicit operator string(SearchInfo SI)
+            {
+                return SI.ItemName;
+            }
+            public string ItemName { get; set; }
+            public int ItemId { get; set; }
+        }
+    }
+}
--- ViewModels/SearchUsrCtrlViewModel.cs
+++ ViewModels/SearchUsrCtrlViewModel.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using CraftHelper.Models;
 using CraftHelper.Views;
 using CraftHelper.ViewModels.Base;
 
@@ -13,10 +14,56 @@ namespace CraftHelper.ViewModels
     public class SearchUsrCtrlViewModel : ViewModelBase
     {
         private readonly UserControl _this;
+        private bool _isInput = false;
+        private string _TextBoxContent = "请输入要搜索的物品名称";
+        private string _TextBoxColor = "Gray";
+        public ICommandBase SearchListBoxDoubleClickCommand { get; set; }
+        public ICommandBase TextBoxGotFocusCommand { get; set; }
 
         public SearchUsrCtrlViewModel(UserControl main)
         {
             _this = main;
+            SearchListBoxDoubleClickCommand = new ICommandBase((object param) => SearchListBoxDoubleClick(param as ListBox));
+            TextBoxGotFocusCommand = new ICommandBase(TextBoxGotFocus);
+        }
+
+        private void SearchListBoxDoubleClick(ListBox obj) 
+        {
+            Console.WriteLine(obj.SelectedIndex);
+        }
+
+        private void TextBoxGotFocus()
+        {
+            if(!_isInput)
+            {
+                _isInput = true;
+                TextBoxContent = "";
+                TextBoxColor = "Black";
+            }
+        }
+
+        public List<SearchModel.SearchInfo> SearchResultTable
+        {
+            get { return SearchModel.SearchResult; }
+            set { SearchModel.SearchResult = value; OnPropertyChanged(); }
+        }
+
+        public List<SearchModel.SearchInfo> SearchAdditionTable
+        {
+            get { return SearchModel.SearchAddition; }
+            set { SearchModel.SearchAddition = value; OnPropertyChanged(); }
+        }
+
+        public string TextBoxContent
+        {
+            get { return _TextBoxContent; }
+            set { _TextBoxContent = value; OnPropertyChanged(); }
+        }
+
+        public string TextBoxColor
+        {
+            get { return _TextBoxColor; }
+            set { _TextBoxColor = value; OnPropertyChanged(); }
         }
     }
 }
